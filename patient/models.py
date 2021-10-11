7112a1d8325720c5efe7971dbe12a759fcd3e219
ahrmada95 <ahrmada95@gmail.com>
2021-10-11 17:50:31 -0400

    django structure

---
+++ patient/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+Class patient
+    int id
+    int user_class
+    string f_name
+    string l_name
+    array string[][]
---
+++ phys_orders/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PhysOrdersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'phys_orders'
