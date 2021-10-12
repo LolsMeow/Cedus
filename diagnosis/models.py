221df538c459fc29f9522dd1140a76a6e7aa4712
LolsMeow <jzheng1818@bths.edu>
2021-10-12 00:14:05 -0400

    Started on models

--- diagnosis/models.py
+++ diagnosis/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Diagnosis(models.Model):
+    Diagnosis_Date = models.DateField()
+    Diagnosis_Status = models.DateField()
+    Diagnosis_Comment = models.DateField()
--- patient/models.py
+++ patient/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
-Class patient
-    int id
-    int user_class
-    string f_name
-    string l_name
-    array string[][]
+class Patient(models.Model):
+    P_FullName = models.CharField(max_length=50)
+    P_DOB = models.DateField()
+    P_Gender = models.CharField(max_length=50)
+    P_Address = models.CharField(max_length=50)
+    P_City = models.CharField(max_length=50)
+    P_State = models.CharField(max_length=50)
+    P_Language = models.CharField(max_length=50)
