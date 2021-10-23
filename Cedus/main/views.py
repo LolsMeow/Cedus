18aa5181647ad0a7d4d7e3acf5d19a010b362b3c
ahrmada95 <ahrmada95@gmail.com>
2021-10-22 23:35:57 -0400

    models and views

--- Cedus/main/views.py
+++ Cedus/main/views.py
@@ -54,18 +54,84 @@ def auth_logout(request):
     return redirect('main:dashboard')
 
 
-def dashboard(request):
+def dashboard_pt(request):
     if not request.user.is_active:
         return redirect('main:login')
-    p = Patient.objects.get(id=1)
-    form = updateInfo(instance = p)
+    p = Patient.objects.get(u_name=user)
+    form = updateInfo(instance=p)
     if request.method == 'POST':
-        p = Patient.objects.all().get(email=request.user)
+        p = Patient.objects.all().get(u_name=user)
         form = updateInfo(data=request.POST, instance=p)
-        print(form)
         p = form.save()
-    return render(request, 'main/dashboard.html', {'form':form})
+    return render(request, 'main/dashboard_pt.html', {'form': form})
 
+
+def dashboard_staff(request):
+    if not request.user.is_active:
+        return redirect('main:login')
+    s = Staff.objects.get(u_name=user)
+    form = updateInfo(instance=s)
+    if request.method == 'POST':
+        s = Staff.objects.all().get(u_name=user)
+        form = updateInfo(data=request.POST, instance=s)
+        s = form.save()
+    return render(request, 'main/dashboard_staff.html', {'form': form})
+
+
+def vitals_pt(request):
+    if not request.user.is_active:
+        return redirect('main:login')
+    if request.method == 'POST':
+        vital_data = Vitals.objects.all().filter(u_name=user)
+        vital_entry = {'vitalData': vital_data}
+    return render(request, 'main/vitals_test.html', vital_entry)
+
+
+def diagnosis_pt(request):
+    if not request.user.is_active:
+        return redirect('main:login')
+    if request.method == 'POST':
+        diag_data = Diagnosis.objects.all().filter(u_name=user)
+        diag_entry = {'diagData': diag_data}
+    return render(request, 'main/diagnosis_test.html', diag_entry)
+
+
+def phys_orders_pt(request):
+    if not request.user.is_active:
+        return redirect('main:login')
+    if request.method == 'POST':
+        phys_orders_data = Phys_Orders.objects.all().filter(u_name=user)
+        phys_orders_entry = {'physOrders': phys_orders_data}
+    return render(request, 'main/phys_orders_test.html', phys_orders_entry)
+
+
+def prescription_pt(request):
+    if not request.user.is_active:
+        return redirect('main:login')
+    if request.method == 'POST':
+        rx_data = Prescription.objects.all().filter(u_name=user)
+        rx_entry = {'rxData': rx_data}
+    return render(request, 'main/prescription_test.html', rx_entry)
+
+
+def vaccinations_pt(request):
+    if not request.user.is_active:
+        return redirect('main:login')
+    if request.method == 'POST':
+        vac_data = Vaccines.objects.all().filter(u_name=user)
+        vac_entry = {'vaccine': vac_data}
+    return render(request, 'main/vaccine_test.html', vac_entry)
+
+def records_pt(request):
+    if not request.user.is_active:
+        return redirect('main:login')
+    if request.method == 'POST':
+        surgery_data = Surgeries.objects.all().filter(u_name=user)
+        bill_data = Bills.objects.all().filter(u_name=user)
+        payment_data = Payment.objects.all().filter(u_name=user)
+
+        records_entry = {'surgeryData': surgery_data, 'billData': bill_data, 'payData': payment_data}
+    return render(request, 'main/records_test.html', records_entry)
 
 def information_request(request):
     if not request.user.is_active:
@@ -77,5 +143,3 @@ def information_request(request):
         return redirect('main:login')
     form = infoReg()
     return render(request=request, template_name='main/info.html', context={'information_form': form})
-
-
